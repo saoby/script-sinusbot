8bacceef0f5f5b9112694c75e658abeaddea8574
saoby <juanher191949@hotmail.com>
2018-11-28 14:29:14 -0500

    Update jaja.sh

--- jaja.sh
+++ jaja.sh
@@ -16,7 +16,7 @@ echo -n $'\E[37m'
 
 sleep 3s
 echo -n $'\E[33m' "Actualizando paquetes ..."; apt-get update > /dev/null; echo $'\E[32m' " Completado.";
-echo -n $'\E[33m' "Instalando paquetes ..."; apt-get install -y x11vnc xvfb libxcursor1 ca-certificates bzip2 libnss3 libegl1-mesa x11-xkb-utils libasound2 libglib2.0-0 python nano screen > /dev/null; echo $'\E[32m' " Completado.";
+echo -n $'\E[33m' "Instalando paquetes ..."; apt-get install -y x11vnc xvfb libxcursor1 ca-certificates bzip2 libnss3 libegl1-mesa x11-xkb-utils libasound2 libglib2.0-0 python nano screen zip > /dev/null; echo $'\E[32m' " Completado.";
 echo -n $'\E[33m' "Actualizando certificados ..."; update-ca-certificates > /dev/null; echo $'\E[32m' " Completado.";
 echo -n $'\E[33m' "Creando usuario ..."; adduser --disabled-password --gecos "" sinusbot > /dev/null; echo $'\E[32m' " Completado.";
 echo -n $'\E[33m' "Creando directorio ..."; mkdir -p /opt/ts3soundboard > /dev/null; echo $'\E[32m' " Completado.";
@@ -34,9 +34,9 @@ cp plugin/libsoundbot_plugin.so TeamSpeak3-Client-linux_amd64/plugins/ > /dev/nu
 chmod 755 sinusbot > /dev/null;
 sed -i '4d' config.ini  > /dev/null;
 echo YoutubeDLPath = "/usr/local/bin/youtube-dl" >> config.ini  > /dev/null;
-echo -n $'\E[33m' "Descargando yt-dl ..."; sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl > /dev/null; echo $'\E[32m' " Completado.";
+echo -n $'\E[33m' "Descargando yt-dl ..."; sudo wget -q https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl > /dev/null; echo $'\E[32m' " Completado.";
 sudo chmod a+rx /usr/local/bin/youtube-dl > /dev/null;
 cd /opt/ts3soundboard/scripts > /dev/null;
-echo -n $'\E[33m' "Descargando scripts ..."; sudo wget -q https://raw.githubusercontent.com/saoby/script-sinusbot/master/scripts.tar.bz2 > /dev/null; echo $'\E[32m' " Completado.";
-tar -xjf scripts.tar.bz2  > /dev/null;
+echo -n $'\E[33m' "Descargando scripts ..."; sudo wget -q https://github.com/saoby/script-sinusbot/raw/master/scripts.zip > /dev/null; echo $'\E[32m' " Completado.";
+unzip scripts.zip  > /dev/null;
 echo -n $'\E[37m'
