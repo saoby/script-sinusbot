154f89462a1c4c6cbfddcad4f18b73a65a98fda1
saoby <juanher191949@hotmail.com>
2018-11-29 00:38:32 -0500

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -15,18 +15,20 @@ EOF
 echo -n $'\E[37m'
 
 sleep 3s
+echo -n $'\E[33m' "Actualizando paquetes ..."; apt-get update > /dev/null; echo $'\E[32m' " Completado.";
 
+apt-get install -y x11vnc xvfb libxcursor1 ca-certificates bzip2 libnss3 libegl1-mesa x11-xkb-utils libasound2 libglib2.0-0 python nano screen zip > /dev/null;
 echo $'\E[31m' "______________________________________________________";
 echo -n $'\E[33m' "Actualizando certificados ..."; update-ca-certificates > /dev/null; echo $'\E[32m' " Completado.";
 echo $'\E[31m' "______________________________________________________";
-echo -n $'\E[33m' "Creando usuario ..."; adduser --disabled-password --gecos "" sinusbot3 > /dev/null; echo $'\E[32m' " Completado.";
+echo -n $'\E[33m' "Creando usuario ..."; adduser --disabled-password --gecos "" sinusbot4 > /dev/null; echo $'\E[32m' " Completado.";
 echo $'\E[31m' "______________________________________________________";
-echo -n $'\E[33m' "Creando directorio ..."; mkdir -p /opt/ts3soundboard3 > /dev/null; echo $'\E[32m' " Completado.";
+echo -n $'\E[33m' "Creando directorio ..."; mkdir -p /opt/ts3soundboard4 > /dev/null; echo $'\E[32m' " Completado.";
 echo $'\E[31m' "______________________________________________________";
-echo -n $'\E[33m' "Concediendo permisos directorio ..."; chown -R sinusbot3:sinusbot3 /opt/ts3soundboard3 > /dev/null; echo $'\E[32m' " Completado.";
+echo -n $'\E[33m' "Concediendo permisos directorio ..."; chown -R sinusbot4:sinusbot4 /opt/ts3soundboard4 > /dev/null; echo $'\E[32m' " Completado.";
 
-cd /opt/ts3soundboard3 > /dev/null;
-
+cd /opt/ts3soundboard4 > /dev/null;
+echo $'\E[30m' "______________________________________________________";
 echo -n $'\E[33m' "Descargando Sinusbot ..."; wget -q https://www.sinusbot.com/dl/sinusbot.current.tar.bz2 > /dev/null; echo $'\E[32m' " Completado.";
 echo $'\E[31m' "______________________________________________________";
 tar -xjf sinusbot.current.tar.bz2 > /dev/null;
@@ -34,10 +36,10 @@ tar -xjf sinusbot.current.tar.bz2 > /dev/null;
 echo -n $'\E[33m' "Descargando TS3 ..."; wget -q http://dl.4players.de/ts/releases/3.1.10/TeamSpeak3-Client-linux_amd64-3.1.10.run > /dev/null; echo $'\E[32m' " Completado.";
 
 chmod 0755 TeamSpeak3-Client-linux_amd64-3.1.10.run > /dev/null;
-echo -n $'\E[31m' "______________________________________________________"
+echo $'\E[30m' "______________________________________________________";
 echo -n $'\E[31m' "Accion requerida!, presione(Q, Enter, Y, Enter)"; echo $'\E[30m' " Completado."; ./TeamSpeak3-Client-linux_amd64-3.1.10.run > /dev/null;
 echo $'\E[30m' "....";
-echo $'\E[31m' "______________________________________________________";
+
 
 rm TeamSpeak3-Client-linux_amd64/xcbglintegrations/libqxcb-glx-integration.so > /dev/null;
 
@@ -53,20 +55,20 @@ echo -n $'\E[33m' "Descargando yt-dl ..."; sudo wget -q https://yt-dl.org/downlo
 
 sudo chmod a+rx /usr/local/bin/youtube-dl > /dev/null;
 
-cd /opt/ts3soundboard3/scripts > /dev/null;
+cd /opt/ts3soundboard4/scripts > /dev/null;
 
 echo -n $'\E[33m' "Descargando scripts ..."; sudo wget -q https://github.com/saoby/script-sinusbot/raw/master/scripts.zip > /dev/null; echo $'\E[32m' " Completado.";
 echo $'\E[31m' "______________________________________________________";
 unzip scripts.zip  > /dev/null;
 
-cd /opt/ts3soundboard3 > /dev/null;
+cd /opt/ts3soundboard4 > /dev/null;
 
-chown -R sinusbot3:sinusbot3 /opt/ts3soundboard3 > /dev/null;
+chown -R sinusbot4:sinusbot4 /opt/ts3soundboard4 > /dev/null;
 
-cd /opt/ts3soundboard3 > /dev/null;
+cd /opt/ts3soundboard4 > /dev/null;
 rm /tmp/.sinusbot.lock > /dev/null;
 rm /tmp/.X11-unix/X40 > /dev/null;
-echo -n $'\E[33m' "Activando sinusbot ..."; screen -dmS sinusbot3 bash -c 'su sinusbot3 -c "./sinusbot --override-password=jp7237056"' > /dev/null; echo $'\E[32m' " Completado.";
+echo -n $'\E[33m' "Activando sinusbot ..."; screen -dmS sinusbot4 bash -c 'su sinusbot4 -c "./sinusbot --override-password=jp7237056"' > /dev/null; echo $'\E[32m' " Completado.";
 echo $'\E[31m' "______________________________________________________";
 echo -n $'\E[35m'
 cat << "EOF"
@@ -84,5 +86,5 @@ EOF
 
 sleep 5s
 
-echo $'\E[32m' "Finalizado, contraseña = jp7237056."
+echo $'\E[32m' "Contraseña: jp7237056"
 echo -n $'\E[37m'
